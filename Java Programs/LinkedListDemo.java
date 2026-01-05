ebe4f0cf4a732ac7b8b4b6daf432d4ddc34b5aa1
abjoe-t <abjoetibi@gmail.com>
2026-01-05 11:21:46 +0530

    Create LinkedListDemo.java

---
+++ Java Programs/LinkedListDemo.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+        list.add("A");
+        list.add("B");
+        System.out.println(list);
+    }
+}
