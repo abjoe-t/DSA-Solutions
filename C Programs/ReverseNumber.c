7564b8c1dbedb5b9fb8d8109566556f324964d23
abjoe-t <abjoetibi@gmail.com>
2026-01-05 11:17:57 +0530

    Add program to reverse a number in C

---
+++ C Programs/ReverseNumber.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n = 1234, reverse = 0, remainder;
+    while (n != 0) {
+        remainder = n % 10;
+        reverse = reverse * 10 + remainder;
+        n /= 10;
+    }
+    printf("Reversed Number: %d", reverse);
+    return 0;
+}
